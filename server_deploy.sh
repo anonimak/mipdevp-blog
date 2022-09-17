2601d1f48e82b4fa28c88c4ccc0a645cf6432639
anonimak <sanonimak@gmail.com>
2022-09-18 04:08:44 +0700

    🖥️ wip: testing deploy

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,6 +6,6 @@ echo "Deploying application ..."
     # Update codebase
     git fetch origin deploy
     git reset --hard origin/deploy
-
+    sudo cp -rf dist/* /var/www/server.jonatan-teofilus.my.id/
 
 echo "Application deployed!"
