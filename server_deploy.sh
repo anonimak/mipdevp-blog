7d9e3ebcab43ee8bc5bceb1e88c6c10065c6f4a8
anonimak <sanonimak@gmail.com>
2022-09-23 10:23:58 +0700

    🐞 fix: change server_deploy file and path

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,6 +6,6 @@ echo "Deploying application ..."
     # Update codebase
     git fetch origin deploy
     git reset --hard origin/deploy
-
+    cp -rf dist/* ~/www/blog
 
 echo "Application deployed!"
