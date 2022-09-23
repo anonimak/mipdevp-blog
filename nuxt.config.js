3e7e20f8413275a42835ad41aa9ec7eb5d7bfc4e
anonimak <sanonimak@gmail.com>
2022-09-23 10:53:59 +0700

    🐞 fix: change base router path

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,10 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  router: {
+    base: '/blog/',
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/css/main.css'],
 
