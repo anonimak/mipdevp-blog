12cbc834a0cd958d77f950362757ec3993d517a4
anonimak <sanonimak@gmail.com>
2022-09-25 21:40:57 +0700

    ✨feature: add meta on article

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -60,5 +60,34 @@ export default {
     // return the data to be vailable for use in the file
     return { article, prev, next, disqusConfig }
   },
+
+  head() {
+    return {
+      title: this.article.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.article.description,
+        }, // Open Graph
+        { hid: 'og:title', property: 'og:title', content: this.article.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.article.description,
+        }, // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.article.title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.article.description,
+        },
+      ],
+    }
+  },
 }
 </script>
