d25c45f2a0abc6a19ad6bd25772ea3dee284e6f7
anonimak <sanonimak@gmail.com>
2022-09-27 09:59:27 +0700

    ✨feature: change navigation color

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -23,10 +23,8 @@
         ><svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="-5 -5 24 24"
-          width="24"
-          height="24"
           preserveAspectRatio="xMinYMin"
-          class="icon jam jam-arrow-left"
+          class="w-8 h-8 dark:bg-gray-300"
         >
           <path
             d="M3.414 7.657l3.95 3.95A1 1 0 0 1 5.95 13.02L.293 7.364a.997.997 0 0 1 0-1.414L5.95.293a1 1 0 1 1 1.414 1.414l-3.95 3.95H13a1 1 0 0 1 0 2H3.414z"
@@ -48,10 +46,8 @@
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="-5 -5 24 24"
-          width="24"
-          height="24"
           preserveAspectRatio="xMinYMin"
-          class="icon jam jam-arrow-right"
+          class="w-8 h-8 dark:bg-gray-300"
         >
           <path
             d="M10.586 5.657l-3.95-3.95A1 1 0 0 1 8.05.293l5.657 5.657a.997.997 0 0 1 0 1.414L8.05 13.021a1 1 0 1 1-1.414-1.414l3.95-3.95H1a1 1 0 1 1 0-2h9.586z"
