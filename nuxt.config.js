655dee742c714bc8788dc4e4b9144fe9d1271b7f
anonimak <sanonimak@gmail.com>
2022-10-11 17:36:07 +0700

    🐞 fix: change title head

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mipdevp-blog',
+    title: 'mipdevp blog - Opini, Tutorial, Panduan',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
