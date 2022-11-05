471fb3bb5f032f287fa602d12c2af36805b173e4
GitHub Action <action@github.com>
2022-11-05 17:06:08 +0000

    Build front-end assets

--- dist/_nuxt/static/1667666608/blog/kedua-ini/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667666608",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fkedua-ini",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cf611e32"}}}}(null));
