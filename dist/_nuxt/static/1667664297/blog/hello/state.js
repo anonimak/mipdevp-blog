55dd6bb13a8d9481e7768187ebdd3f54ab96fe9b
GitHub Action <action@github.com>
2022-11-05 16:43:28 +0000

    Build front-end assets

--- dist/_nuxt/static/1667664297/blog/hello/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667664297",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhello",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62dcc7c8"}}}}(null));
