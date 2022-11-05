35a25d402986b043f84f1813bfbd10bb2970e1ec
GitHub Action <action@github.com>
2022-11-05 17:11:33 +0000

    Build front-end assets

---
+++ dist/_nuxt/static/1667668293/blog/atikel-baru/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667668293",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fatikel-baru",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a28652d4"}}}}(null));
